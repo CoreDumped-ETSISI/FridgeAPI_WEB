0c8c4eec023411a60d5b144b4152f18ace3f777e
pabloVoorvaart <pablov@graficadirecta.com>
2019-02-03 20:58:54 +0100

    added offer functionality to the store frontend: show offers / add to cart / delete from cart. Integrated offer functionality with existing products functionality. Missing: implement offers to the purchase function in public/store.js

--- public/js/store.js
+++ public/js/store.js
@@ -1,6 +1,6 @@
 let productList;
 
-let cart = [];
+let products = [];
 let offers = [];
 let total = 0;
 
@@ -37,7 +37,7 @@ function createProductCards() {
     }
 
     for (let i = 0; i < productList.length; i++) {
-        products.append(productCard(productList[i]));
+        products.append(card(productList[i], 'addProductToCart'));
     }
 
     setTimeout(() => {
@@ -54,7 +54,7 @@ function createOfferCards() {
     }
 
     for (let i = 0; i < offerList.length; i++) {
-      offers.append(offerCard(offerList[i]));
+      offers.append(card(offerList[i], 'addOfferToCart'));
     }
 
     setTimeout(() => {
@@ -62,22 +62,15 @@ function createOfferCards() {
     }, 500);
 }
 
-function productCard(product) {
-  return card(product, 'addToCart');
-}
 
-function offerCard(offer) {
-  return card(offer, 'addOfferToCart');
-}
-
-function card(product, funcName) {
+function card(element, funcName) {
   return `<div class="col s6 m4 l3 xl2">
           <div class="card scale-transition waves-effect waves-green scale-out product-item">
-          <div id="${product._id}" class="card-image" onclick="${funcName}('${product._id}')">
-          <img src="${product.image}"/>
-          <span class="card-title" id="product-name">${product.name}</span>
-          <span class="new badge" id="product-stock" data-badge-caption=" UDS ">${product.stock}</span>
-          <a class="btn red" id="product-price">${product.price}€</a></div></div></div>`
+          <div id="${element._id}" class="card-image" onclick="${funcName}('${element._id}')">
+          <img src="${element.image}"/>
+          <span class="card-title" id="product-name">${element.name}</span>
+          <span class="new badge" id="product-stock" data-badge-caption=" UDS ">${element.stock}</span>
+          <a class="btn red" id="product-price">${element.price}€</a></div></div></div>`
 }
 
 function title(name) {
@@ -88,19 +81,37 @@ function title(name) {
 }
 
 function addOfferToCart(id) {
-    let offer = offerList.filter((elem) => {
-        return elem._id === id;
-    })[0];
+    let offerStock = parseInt($('#' + id).find('.badge').text());
+    if (offerStock > 0) {
+        $("#empty").empty();
 
-    offers.push(offer);
+        let offer = offerList.filter((elem) => {
+            return elem._id === id;
+        })[0];
 
-    for (let i = 0; i < offer.products.length; i++){
+        var is_stock = false;
+        for (let i = 0; i < offer.products.length; i++){
+          let productStock = $('#' + offer.products[i].product._id).find('.badge');
+          let stock = parseInt(productStock.text());
+          if(stock >= offer.products[i].quantity){ is_stock = true; }
+          else { is_stock = false; }
+        }
 
-      updateStock(offer.products[i].product._id, -offer.products[i].quantity);
-    }
+        if (is_stock === true){
+          for (let i = 0; i < offer.products.length; i++){
+            updateStock(offer.products[i].product._id, -offer.products[i].quantity);
+        }
+          offers.push(offer);
+          updateStock(id, -1);
 
+          M.toast({html: `${offer.name} añadido al carrito`, classes: 'blue'});
+          createCartElement(offer, (offers.length - 1), 'eraseOfferElement');
+          reloadCart();
+        }
+      }
 }
-function addToCart(id) {
+
+function addProductToCart(id) {
     let productStock = $('#' + id).find('.badge');
     let stock = parseInt(productStock.text());
     if (stock > 0) {
@@ -109,43 +120,41 @@ function addToCart(id) {
         let product = productList.filter((elem) => {
             return elem._id === id;
         })[0];
-        cart.push(product);
+        products.push(product);
         updateStock(id, -1);
 
         M.toast({html: `${product.name} añadido al carrito`, classes: 'blue'});
-        createCartElement(product, (cart.length - 1));
+        createCartElement(product, (products.length - 1), 'eraseProductElement');
         reloadCart();
     }
 }
 
-function reloadCart() {
-    $("#cartList").empty();
+function createCartElement(element, index, eraseFuncName) {
+    $("#cartList").append(
+     `<li class="cartElement valign-wrapper">
+      <img class="responsive-img" src="${element.image}" style="width: 15%; margin-left: 5%; object-fit: contain;">
+      <span style="width: 65%; padding-left: 1em;">${element.name}</span>
+      <i class="material-icons" onclick="${eraseFuncName}('${element._id}', '${index}', '${element.name}')" style="cursor: pointer;">clear</i>
+      </li>`);
 
-    if (cart.length === 0) {
-        listIsEmpty();
-    }
-    else {
-        for (let i = 0; i < cart.length; i++) {
-            createCartElement(cart[i], i);
-        }
-    }
-
-    reloadCartCost();
 }
 
-function createCartElement(product, index) {
-    $("#cartList").append('<li class="cartElement valign-wrapper">' +
-        '    <img class="responsive-img" src="' + product.image + '" style="width: 15%; margin-left: 5%; object-fit: contain;">' +
-        '    <span style="width: 65%; padding-left: 1em;">' + product.name + '</span>' +
-        '    <i class="material-icons" onclick="eraseCartElement(\'' + product._id + '\','+ index + ',\''+ product.name +'\')" style="cursor: pointer;">clear</i>' +
-        '</li>');
-}
-
-function eraseCartElement(id, index, name) {
+function eraseProductElement(id, index, name) {
     M.toast({html: `${name} eliminado del carrito`, classes: 'blue'});
-    cart.splice(index, 1);
+    products.splice(index, 1);
     updateStock(id, 1);
     reloadCart();
+}
+
+function eraseOfferElement(id, index, name) {
+  let offer = offers[index];
+  M.toast({html: `${name} eliminado del carrito`, classes: 'blue'});
+  for (let i = 0; i < offer.products.length; i++){
+    updateStock(offer.products[i].product._id, offer.products[i].quantity);
+  }
+  updateStock(id, 1);
+  offers.splice(index, 1);
+  reloadCart();
 }
 
 function updateStock(productId, quantity) {
@@ -160,27 +169,49 @@ function reloadCartCost() {
     totalElem.empty();
     total = 0;
 
-    for (let i = 0; i < cart.length; i++) {
-        total += cart[i].price;
+    for (let i = 0; i < products.length; i++) {
+        total += products[i].price;
+    }
+
+    for (let i = 0; i < offers.length; i++) {
+        total += offers[i].price;
     }
 
     totalElem.append((Math.round(total * 100) / 100) + ' €');
 }
 
+function reloadCart() {
+    $("#cartList").empty();
+
+    if (products.length === 0 && offers.length === 0) {
+        listIsEmpty();
+    }
+    else {
+        for (let i = 0; i < products.length; i++) {
+            createCartElement(products[i], i, 'eraseProductElement');
+        }
+        for (let i = 0; i < offers.length; i++) {
+            createCartElement(offers[i], i, 'eraseOfferElement');
+        }
+    }
+
+    reloadCartCost();
+}
+
 function purchase() {
-    if (cart.length > 0) {
+    if (products.length > 0) {
         //if ((Math.round(total * 100) / 100) <= (Math.round(user.balance * 100) / 100)) {
             let itemChain = "";
             let offerItemChain = "";
 
-            for (let i = 0; i < cart.length - 1; i++)
-                itemChain += cart[i]._id + ",";
-            itemChain = itemChain + cart[cart.length - 1]._id;
+            for (let i = 0; i < products.length - 1; i++)
+                itemChain += products[i]._id + ",";
+            itemChain = itemChain + products[products.length - 1]._id;
 
             const data = {productList: itemChain, offerList: offerItemChain};
 
             request('POST', '/purchase', data, (res) => {
-                cart = [];
+                products = [];
                 reloadCart();
                 getProfile();
                 getProductList();
